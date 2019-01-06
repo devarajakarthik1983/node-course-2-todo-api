2e7e0f7c9e38b537754bd593e4a95db0cd3423b1
devarajakarthik83 <karthikdevaraja83@gmail.com>
2019-01-06 15:47:21 -0500

    added delete file for mongodb

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,79 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error,client)=>{
+  if(error){
+    return console.log('Unable to connect to the Database Serve');
+  }
+
+  const db = client.db('TodoApp');
+
+// db.collection('TodoApp').find({completedAt:true}).toArray().then((docs)=>{
+// console.log(JSON.stringify(docs,undefined,2));
+// }, (err)=>{
+//   console.log('Umable to detch the docs');
+// });
+
+// db.collection('TodoApp').find().toArray().then((docs)=>{
+// console.log(JSON.stringify(docs,undefined,2));
+// }, (err)=>{
+//   console.log('Umable to detch the docs');
+// });
+
+// db.collection('TodoApp').find({_id: new ObjectID('5c322ed01687dd1b40ea4d1e')}).toArray().then((docs)=>{
+// console.log(JSON.stringify(docs,undefined,2));
+// }, (err)=>{
+//   console.log('Umable to detch the docs');
+// });
+
+
+// db.collection('TodoApp').find().count().then((count)=>{
+// console.log('Count: ',count)
+// }, (err)=>{
+//   console.log('Umable to detch the docs');
+// });
+
+// db.collection('Users').find({name:'Karthik'}).toArray().then((docs)=>{
+// console.log(JSON.stringify(docs,undefined,2));
+// },(err)=>{
+// console.log('Unable to detch the record');
+// });
+
+// db.collection('TodoApp').deleteMany({text:'Karthik'}).then((results)=>{
+//   console.log(results);
+// });
+
+// db.collection('TodoApp').deleteOne({name:'Karthik'}).then((results)=>{
+//   console.log(results);
+// });
+//
+// db.collection('TodoApp').findOneAndDelete({completedAt: false}).then((results)=>{
+//   console.log(results);
+// });
+
+db.collection('Users').deleteMany({name:'Karthik'}).then((res)=>{
+  console.log(res);
+} , (err)=>{
+  console.log(err);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //client.close();
+
+})
