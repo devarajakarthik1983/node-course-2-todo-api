3c16c531965b2782be5b17892807aae038815b45
devarajakarthik83 <karthikdevaraja83@gmail.com>
2019-01-08 19:20:08 -0500

    Added test for delete todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -111,8 +111,45 @@ request(app)
 .expect(404)
 .end(done);
 });
+});
 
 
+describe('DELETE Todos',()=>{
+it('should delete the todo' , (done)=>{
+  var hexId=todos[1]._id.toHexString();
+  request(app)
+  .delete(`/todos/${hexId}`)
+  .expect(200)
+  .expect((res)=>{
+    expect(res.body.todo._id).toBe(hexId);
+  })
+  .end((err,res)=>{
+    if(err){
+      return done(err);
+    }
 
+    Todo.findById(hexId).then((todo)=>{
+      expect(todo._id.toHexString()).toBe(hexId);
+      done();
+    }).catch((e)=>{
+      done(e);
+    });
+  });
 
+});
+});
+
+it('should return 404 if the id not found' , (done)=>{
+var id ='6c34cd2d5847ba8ae9dbcaee';
+request(app)
+.delete('/todos/id')
+.expect(404)
+.end(done);
+});
+
+it('should return 404 if the objectid is invalid' , (done)=>{
+request(app)
+.delete('/todos/123')
+.expect(404)
+.end(done);
 });
