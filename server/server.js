0cb666590b64cfbc0351b38eaf198e19f7bd3cca
devarajakarthik83 <karthikdevaraja83@gmail.com>
2019-01-08 20:00:13 -0500

    Added update todo by ID

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
+const _ =require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -71,6 +72,37 @@ if(!todo) {
   res.status(404).send();
 });
 });
+
+app.patch('/todos/:id' ,(req,res)=>{
+  var id =  req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+
+  if(!ObjectID.isValid(id)) {
+  return  res.status(404).send();
+}
+
+
+  if(_.isBoolean(body.completed) && body.completed) {
+      body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false,
+    body.completedAt =null;
+  }
+
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+      if(!todo){
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+});
+
+
+
+
 
 
 
